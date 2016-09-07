a8698b5a746635e84dffd16a0bc6e0ddfe26a6c8
DNCrosby <DNCrosby@users.noreply.github.com>
2016-09-07 12:26:09 -0700

    Update ViveVCR.cs
    Revision 2 Adds proper Shuttle and Play Functionality, but still has some Issues with Jog, that I will resolve soon.

--- ViveVCR.cs
+++ ViveVCR.cs
@@ -1,6 +1,14 @@
+// Written By : Derek Crosby
+// Copyright 09/2016
+// Last Updated : 9/6/2016
+
+// Vive VCR Controller, to add functionality for scrubbing through animation for animation reviews in VR
+// Uage: Drop onto Vive Controller, and drag in an animation object. If Neccessary change the animation name that needs to be played back.
+// Currently only works with one animation.
+// TODO: Add Animation List to be able to play all animations at once, or sequence through them.
+
 using UnityEngine;
 using System.Collections;
-//using UnityEngine.Networking;
 
 public class ViveVCR : MonoBehaviour
 {
@@ -9,40 +17,52 @@ public class ViveVCR : MonoBehaviour
     [SerializeField]
     SteamVR_TrackedObject trackedObj;
     SteamVR_Controller.Device device;
-    private const int mMessageWidth = 200;
-    private const int mMessageHeight = 64;
 
-    private readonly Vector2 mXAxis = new Vector2(1, 0);
-    private readonly Vector2 mYAxis = new Vector2(0, 1);
-    private bool trackingSwipe = false;
-    private bool checkSwipe = false;
+    //VTR Parameters 
+    public int maxPlaybackSpeed = 3;
+    public int frameAdvancePerTurn = 16;
 
-    // To recognize as swipe user should at lease swipe for this many pixels
-    private const float mMinSwipeDist = 0.2f;
+    private bool updatePosition = false;
 
+    // Jog Shuttle Play Controlls
+    private enum JogShuttle { jog, shuttle, play };
+    private JogShuttle jogShuttle;
+    private float old_controlRotation = 0;
+    private float controlRotation;
+    private float playbackPosition;
     private Vector2 mStartPosition;
-    private Vector2 endPosition;
+    private Vector2 mEndPosition;
 
-    private float mSwipeStartTime;
     void Awake()
     {
         if (trackedObj == null)
         {
             trackedObj = GetComponent<SteamVR_TrackedObject>();
         }
-        
+
         if (anim == null)
         {
             anim = GameObject.FindObjectOfType<Animation>();
         }
-        
+
     }
 
 
     // Use this for initialization
     void Start()
     {
+        controlRotation = 0;
         device = SteamVR_Controller.Input((int)trackedObj.index);
+        jogShuttle = JogShuttle.play;
+        PlayAnimationFromPosition(0f, 0f);
+    }
+
+
+    // Utility for getting angle from pad.
+    public static float GetAngleDegree(Vector2 origin, Vector2 target)
+    {
+        var n = 270 - (Mathf.Atan2(origin.y - target.y, origin.x - target.x)) * 180 / Mathf.PI;
+        return n % 360;
     }
 
     // Update is called once per frame
@@ -50,64 +70,156 @@ public class ViveVCR : MonoBehaviour
     {
         if (trackedObj != null && anim != null)
         {
-            // Touch down, possible chance for a swipe
+
+            // Check / Change If Jog Or Shuttle Mode With Touch Button
+            if (device.GetPressUp(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+            {
+                if (jogShuttle == JogShuttle.jog)
+                    jogShuttle = JogShuttle.shuttle;
+                else if (jogShuttle == JogShuttle.shuttle)
+                    jogShuttle = JogShuttle.play;
+                else if (jogShuttle == JogShuttle.play)
+                    jogShuttle = JogShuttle.jog;
+
+                Debug.Log("Mode is : " + jogShuttle.ToString());
+            }
+
+
+            // Touch down, possible chance for a beginning position
             if ((int)trackedObj.index != -1 && device.GetTouchDown(Valve.VR.EVRButtonId.k_EButton_Axis0))
             {
-                trackingSwipe = true;
-                // Record start time and position
+                updatePosition = true;
+                // Record start position
                 mStartPosition = new Vector2(device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x,
                     device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y);
-                mSwipeStartTime = Time.time;
+
             }
-            // Touch up , possible chance for a swipe
+            // Touch up , possible chance for a new position
             else if (device.GetTouchUp(Valve.VR.EVRButtonId.k_EButton_Axis0))
             {
-                trackingSwipe = false;
-                trackingSwipe = true;
-                checkSwipe = true;
-                Debug.Log("Tracking Finished");
+                updatePosition = false;
+                updatePosition = true;
+                //Debug.Log("Tracking Finished");
             }
-            else if (trackingSwipe)
+
+            else if (updatePosition)
             {
-                endPosition = new Vector2(device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x,
+                mEndPosition = new Vector2(device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).x,
                                           device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0).y);
 
-            }
+                //if (mEndPosition != new Vector2(0f, 0f))
+                controlRotation = GetAngleDegree(Vector2.zero, mEndPosition);
 
-            if (checkSwipe)
-            {
-                checkSwipe = false;
-
-                float deltaTime = Time.time - mSwipeStartTime;
-
-                Vector2 swipeVector = endPosition - mStartPosition;
-
-                //float velocity = swipeVector.magnitude / deltaTime;
-                //Debug.Log("VELOCITY : " + velocity);
-                if (//velocity > mMinVelocity &&
-                    swipeVector.magnitude > mMinSwipeDist)
+                float delta = 0;
+                switch (jogShuttle)
                 {
-                    // if the swipe has enough velocity and enough distance
+                    case JogShuttle.jog:
+                        //ChangeAnimationPosition(playbackPosition + ((controlRotation / 360) * frameAdvancePerTurn));                        
+                        //Debug.Log("Changed Position to : " + (playbackPosition + (controlRotation * 360 / frameAdvancePerTurn)).ToString());
+                        
+                        if (controlRotation != 0)
+                        {
+                            Debug.Log("Control Rotation is : " + old_controlRotation.ToString());
+                            delta = ((controlRotation - old_controlRotation) % 360) * ((frameAdvancePerTurn / 360)*30);
 
+                        }
+                        else
+                        {
+                            delta = 0;
+                        }
 
-                    swipeVector.Normalize();
+                        ChangeAnimationPosition(delta + playbackPosition);
+                        old_controlRotation = controlRotation;
 
-                    float angleOfSwipe = Vector2.Dot(swipeVector, mXAxis);
-                    angleOfSwipe = Mathf.Acos(angleOfSwipe) * Mathf.Rad2Deg;
-
-
-                    // Todo Need to get name of animation from animation asset.
-                    anim.Play("Take 001");
-                    anim.Stop();
-                    anim["Take 001"].speed = 0;
-                    anim["Take 001"].enabled = true;
-                    anim["Take 001"].time = (1f / 24f) * angleOfSwipe;
-
-                    Debug.Log("Angle : " + angleOfSwipe);
+                        Debug.Log(delta.ToString());
+                        break;
+                    case JogShuttle.shuttle:
+                        if (controlRotation > 240f && controlRotation < 360f)
+                        {
+                            delta = Mathf.DeltaAngle(0f, controlRotation);
+                        }
+                        if (controlRotation > 0f && controlRotation < 90f)
+                        {
+                            delta = Mathf.DeltaAngle(0f, controlRotation);
+                        }
+                        ChangeAnimationPlaybackSpeed((delta / 90) * maxPlaybackSpeed);
+                        Debug.Log("Changed Playback Speed to : " + ((delta / 90) * maxPlaybackSpeed).ToString());
+                        break;
+                    case JogShuttle.play:
+                        if (mEndPosition.x != 0f)
+                        {
+                            Debug.Log("PlaybackPosition entered : " + playbackPosition.ToString());
+                            PlayAnimationFromPosition(playbackPosition, mEndPosition.x);
+                            Debug.Log("Position.x : " + mEndPosition.x.ToString());
+                        }
+                        break;
+                    default:
+                        Debug.LogError("Could not find " + jogShuttle + " in options for JogShuttleSwitch ");
+                        break;
                 }
+                //updatePosition = false;
             }
+            playbackPosition = anim["Take 001"].time;
         }
         else return;
+
+
     }
-    
+
+
+    // Modules to Change Time based on controller input.
+    void ChangeAnimationPlaybackSpeed(float speed)
+    {
+        // Todo Need to get name of animation from animation asset.
+        if (!anim.isPlaying)
+        {
+            anim.Play("Take 001");
+        }
+        anim["Take 001"].speed = speed;
+        anim["Take 001"].enabled = true;
+
+    }
+
+
+    void ChangeAnimationPosition(float time)
+    {
+        anim["Take 001"].enabled = true;
+        // Todo Need to get name of animation from animation asset.
+        if (!anim.isPlaying)
+        {
+            anim.Play("Take 001");
+        }
+        anim["Take 001"].speed = 0;
+        anim["Take 001"].time = time;
+    }
+
+    void PlayAnimationFromPosition(float time, float direction)
+    {
+        // Todo Need to get name of animation from animation asset.
+
+        //anim["Take 001"].time = time;
+        anim["Take 001"].enabled = true;
+        if (direction > .5f)
+        {
+            anim.Play("Take 001");
+            anim["Take 001"].speed = 1;
+        }
+        if (direction < -.5)
+        {
+            anim.Play("Take 001");
+            anim["Take 001"].speed = -1;
+        }
+        if (direction < .5 && direction > -.5)
+        {
+            if (anim["Take 001"].speed == 1)
+            {
+                anim["Take 001"].speed = 0;
+            }
+            else
+            {
+                anim["Take 001"].speed = 1;
+            }
+        }
+
+    }
 }
